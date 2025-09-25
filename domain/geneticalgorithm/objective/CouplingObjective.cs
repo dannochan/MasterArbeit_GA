9b799d53f22a63b14716604b561ee01810ac29a4
DannoChan <dawsonchan0508@gmail.com>
2025-09-25 11:49:57 +0200

    refactor: coupling objective calculation logic

--- domain/geneticalgorithm/objective/CouplingObjective.cs
+++ domain/geneticalgorithm/objective/CouplingObjective.cs
@@ -15,24 +15,33 @@ public class CouplingObjective : Objective
     }
     public override double CalculateValue(List<Module> modules)
     {
+        var moduleList = modules.Where(module => !ModuleInformationService.IsIsolated(module, graph)).ToList();
         var visitedEdges = new HashSet<IObjectRelation>();
         double totalCoupling = 0.0;
+        double sumOfModuleEdgesWeights = 0.0;
 
-        foreach (var module in modules)
+        foreach (var module in moduleList)
         {
             if (ModuleInformationService.IsIsolated(module, graph))
                 continue;
 
-            foreach (var edge in ModuleInformationService.GetBoundaryEdgesOfModule(module, graph))
+            var edgesOfModule = ModuleInformationService.GetModuleEdges(module, graph).Where(edge => module.CheckIndexInModule(edge.Source.GetIndex()) && module.CheckIndexInModule(edge.Target.GetIndex())).ToList();
+            sumOfModuleEdgesWeights += edgesOfModule.Sum(edge => edge.Weight);
+
+            var boundaryEdgesOfModuleWithoutBothIO = ModuleInformationService.GetBoundaryEdgesOfModule(module, graph).ToList();
+
+            foreach (var edge in boundaryEdgesOfModuleWithoutBothIO)
             {
-                if (visitedEdges.Add(edge))
+                if (edge != null && !visitedEdges.Contains(edge))
                 {
+                    visitedEdges.Add(edge);
                     totalCoupling += edge.Weight;
                 }
             }
         }
+        double allEdgesWeights = totalCoupling + sumOfModuleEdgesWeights;
 
-        return totalCoupling;
+        return (totalCoupling / allEdgesWeights) * 100.0; // return as percentage
     }
     public override string GetObjectiveName()
     {
